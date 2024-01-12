d99e2d77b5e67565bb5661f61682cbd4c83e4548
hartungvictor <victor_hartung@hotmail.com>
2024-01-12 10:44:11 -0300

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import model.entities.Department;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
 
     }
 }
