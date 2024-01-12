a26e2c79e6eaecdfb42c023523ec8a9b77ee1315
hartungvictor <victor_hartung@hotmail.com>
2024-01-12 16:34:32 -0300

    upd jdbc

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         Department obj = new Department(1, "Books");
         System.out.println(obj);
 
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
